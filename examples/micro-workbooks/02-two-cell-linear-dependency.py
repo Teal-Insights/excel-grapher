464a15c11d340b8682be35c70be8feb2ac9de13c
chriscarrollsmith <christopher.smith@promptlytechnologies.com>
2026-04-23 10:13:54 -0400

    Micro-workbook examples

---
+++ examples/micro-workbooks/02-two-cell-linear-dependency.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from pprint import pprint
+
+from excel_grapher.grapher import create_dependency_graph, to_graphviz
+
+if __name__ == "__main__":
+    # Path to the workbook to extract the graph from
+    WORKBOOK_PATH: Path = Path("examples/micro-workbooks/02-two-cell-linear-dependency.xlsx")
+    TARGET: str = "Sheet1!A2"
+
+    # Extract the graph from the workbook with A2 as the target
+    print(f"Extracting graph from {WORKBOOK_PATH} with target {TARGET}...")
+    graph = create_dependency_graph(WORKBOOK_PATH, [TARGET], load_values=False)
+
+    # Print the graph object's type and field names
+    print(f"\nGraph type: {type(graph)}\nGraph fields: {graph.__dataclass_fields__.keys()}\n")
+
+    # Print the graph
+    pprint(graph, indent=4)
+
+    # Export to graphviz and write to file
+    dot = to_graphviz(graph)
+    with open("examples/micro-workbooks/02-two-cell-linear-dependency.qmd", "w") as f:
+        f.write(f"```{{dot}}\n{dot}\n```")
